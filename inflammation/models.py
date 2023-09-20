775da3c23233ba27a50c72a779cbaed6b9157c7b
josephwj <josephwjerkins@gmail.com>
2023-09-20 10:39:22 -0400

    Added docstrings

--- inflammation/models.py
+++ inflammation/models.py
@@ -13,22 +13,60 @@ import numpy as np
 def load_csv(filename):
     """Load a Numpy array from a CSV
 
-    :param filename: Filename of CSV to load
+    Parameters
+    ----------
+    filename : string
+        Path of csv file to load
+
+    Returns
+    -------
+    np.ndarray of loaded txt
     """
     return np.loadtxt(fname=filename, delimiter=',')
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+
+    Parameters
+    ----------
+    data : list or np.ndarray
+        Data to calculate mean of
+
+    Returns
+    -------
+    float representing mean of data
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    
+    Parameters
+    ----------
+    data : list or np.ndarray of data
+
+    Returns
+    -------
+    Maximum value(s) of data along principal axis. For data w/ shape (a,b,c,...)
+    then returned array will have shape (b,c,...).
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+
+    Parameters
+    ----------
+    data : list or np.ndarray of data
+
+    Returns
+    -------
+    Minimum value(s) of data along principal axis. For data w/ shape (a,b,c,...)
+    then returned array will have shape (b,c,...).
+    -------
+    """
     return np.min(data, axis=0)
 
